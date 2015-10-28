d1c73d2338a8ebf217eb80ef879354054d27b426
Giovani Gonzalez <darkgeat@gmail.com>
2015-10-28 17:44:27 -0600

    Adding ZipCodes and Main Activity, also the navigation and the structure of the project

--- app/src/main/java/com/giovas/weatherapp/ZipCodes.java
+++ app/src/main/java/com/giovas/weatherapp/ZipCodes.java
@@ -4,10 +4,23 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LayoutAnimationController;
+import android.view.animation.ScaleAnimation;
+
+import com.giovas.adapters.ZipCodeAdapter;
+
+import java.util.ArrayList;
 
 public class ZipCodes extends AppCompatActivity {
+
+    private RecyclerView list;
+    private ZipCodeAdapter zipCodeAdapter;
+    private ArrayList<String> items = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +28,23 @@ public class ZipCodes extends AppCompatActivity {
         setContentView(R.layout.activity_zip_codes);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        items.add("99880");
+        items.add("54130");
+        items.add("56789");
+        items.add("12345");
+
+        list = (RecyclerView)findViewById(R.id.list_zipcodes);
+        list.setLayoutManager(new LinearLayoutManager(this));
+        list.setHasFixedSize(true);
+
+        zipCodeAdapter = new ZipCodeAdapter(this,items);
+        list.setAdapter(zipCodeAdapter);
+
+        Animation animation = new ScaleAnimation((float) 1.0, (float) 1.0, (float) 0, (float) 1.0);
+        animation.setDuration(500);
+        LayoutAnimationController controller = new LayoutAnimationController(animation,0.9f);
+        list.setLayoutAnimation(controller);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
