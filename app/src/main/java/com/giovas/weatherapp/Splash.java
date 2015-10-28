6cba82a12c2e7ca139cc0e873cbb70758f9631ec
Giovani Gonzalez <darkgeat@gmail.com>
2015-10-27 19:27:10 -0600

    Adding ZipCodes and Main Activity, also the navigation and the structure of the project

--- app/src/main/java/com/giovas/weatherapp/Splash.java
+++ app/src/main/java/com/giovas/weatherapp/Splash.java
@@ -1,7 +1,11 @@
 package com.giovas.weatherapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class Splash extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ImageView imagen = (ImageView)findViewById(R.id.imageSplash);
+        Glide.with(this).load(R.drawable.art_clear_animated).asGif()
+                .placeholder(R.drawable.art_clear)
+                .into(imagen);
+        Thread tr = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(7000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    Intent intent = new Intent(Splash.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        tr.start();
     }
 }
